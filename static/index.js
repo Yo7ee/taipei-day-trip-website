495161bc37e3cdefaa4a8b15cc7471388d4c1788
yo7ee <yo7chen@gmail.com>
2022-03-08 20:59:03 +0800

    make example for article

--- static/index.js
+++ static/index.js
@@ -1,1 +1,14 @@
-let article=document.querySelectorAll('article');
+// let article=document.querySelectorAll('article');
+let src="http://3.224.188.5:3000/api/attractions";
+const data=fetch(src).then(function(response){
+    return response.json();
+});
+console.log(data)
+data.then(function(jsonobj){
+    let list=jsonobj.data;
+    for (let i=0; i<12; i++){
+        let myfigure=document.createElement('figure');
+        let myimg=document.createElement('img')
+        let myfigcaption=document.createElement('figcaption')
+    }
+})
