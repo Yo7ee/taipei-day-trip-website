f244f30ca6c69761e6ccdff42b70217d9945ab07
yo7ee <yo7chen@gmail.com>
2022-04-16 15:07:56 +0800

    modify thankyou page signout to memberInfo

--- static/js/thankyou.js
+++ static/js/thankyou.js
@@ -16,39 +16,9 @@ fetch(src, {method:'GET'}).then(function(response){
     }
 });
 
-async function checkStatus(){
-    let src="/api/user";
-    let signinNav=document.querySelector('.signinNav');
-    let logoutNav=document.querySelector('.logoutNav');
-    console.log(document.cookie)
-    const response=await fetch(src, {method:'GET'});
-    const data=await response.json();
-    console.log(data.data)
-
-    if(data.data==null){
-        signinNav.style.display="list-item";
-        logoutNav.style.display="none";
-    }else{
-        signinNav.style.display="none";
-        logoutNav.style.display="list-item";
-    };
-};
-
 async function deleteBooking(){
     let cookie=document.cookie
     let src="/api/booking";
     const response= await fetch(src, {method:"DELETE", headers:{'cookie':cookie}});
     const data= await response.json();
-    if (response.status==200){
-        booked.style.display="none";
-        none_booked.style.display="block";
-        html.style.height="100%";
-        body.style.height="100%";
-        footer.style.height="100%";
-        
-        checkStatus();
-    }else{
-        signinNav.style.display="list-item";
-        logoutNav.style.display="none";
-    }
 }
